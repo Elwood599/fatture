6adeb9f5bbd4561f261181d58b4b545ab353ff4c
Elwood599 <mcaputo@power2cloud.com>
2026-01-09 18:38:12 +0100

    Fix prisma

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -33,7 +33,6 @@ CREATE TABLE "Session" (
      "locale" TEXT,
      "collaborator" BOOLEAN DEFAULT false,
      "emailVerified" BOOLEAN DEFAULT false,
-     "refreshToken" TEXT,
-     "refreshTokenExpires" TEXT,
+
      CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
  );
