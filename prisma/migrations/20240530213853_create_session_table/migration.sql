e15def5fd9b7a482da7b04b227a08e19a0259585
Elwood599 <mcaputo@power2cloud.com>
2026-01-10 08:31:05 +0100

    Fix Sassion table

--- prisma/migrations/20240530213853_create_session_table/migration.sql
+++ prisma/migrations/20240530213853_create_session_table/migration.sql
@@ -18,22 +18,25 @@
 -- );
 
 CREATE TABLE "Session" (
-     "id" TEXT NOT NULL,
-     "shop" TEXT NOT NULL,
-     "state" TEXT NOT NULL,
-     "isOnline" BOOLEAN NOT NULL DEFAULT false,
-     "scope" TEXT,
-     "expires" TIMESTAMP WITHOUT TIME ZONE,
-     "accessToken" TEXT NOT NULL,
-     "userId" BIGINT,
-     "firstName" TEXT,
-     "lastName" TEXT,
-     "email" TEXT,
-     "accountOwner" BOOLEAN NOT NULL DEFAULT false,
-     "locale" TEXT,
-     "collaborator" BOOLEAN DEFAULT false,
-     "emailVerified" BOOLEAN DEFAULT false,
-     "refreshToken" TEXT,
-     "refreshTokenExpires" TIMESTAMP WITHOUT TIME ZONE,
-     CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
- );
+  "id" TEXT PRIMARY KEY,
+  "shop" TEXT NOT NULL,
+  "state" TEXT NOT NULL,
+  "isOnline" BOOLEAN NOT NULL DEFAULT false,
+  "scope" TEXT,
+  "expires" TIMESTAMP WITHOUT TIME ZONE,
+
+  "accessToken" TEXT,
+
+  "userId" BIGINT,
+  "firstName" TEXT,
+  "lastName" TEXT,
+  "email" TEXT,
+  "accountOwner" BOOLEAN NOT NULL DEFAULT false,
+  "locale" TEXT,
+  "collaborator" BOOLEAN DEFAULT false,
+  "emailVerified" BOOLEAN DEFAULT false,
+
+  "refreshToken" TEXT,
+  "refreshTokenExpires" TIMESTAMP WITHOUT TIME ZONE
+);
+
